fd9e4f887a95c65de47f9cb667f1bcfd8fac5bc3
Igor Baroni <igorbaroni77@gmail.com>
2024-09-07 11:28:41 -0300

    feat: color codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          darkgreen: "#038C4C",
+          lightgreen: "#04BF68",
+          darkblue: "#021B49",
+          white: "#FFFFEC",
+          gray: "#2D2D2D",
+          yellow: "#F2CB05",
+        },
+      },
+    },
   },
   plugins: [],
 };

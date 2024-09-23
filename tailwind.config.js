02e80b609de88947f3a002d76ee1445774f94797
Igor Baroni <igorbaroni77@gmail.com>
2024-09-23 14:21:16 -0300

    fix: colors fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          darkgreen: "#038C4C",
-          lightgreen: "#04BF68",
-          darkblue: "#021B49",
-          white: "#FFFFEC",
-          darkgray: "#3D3D3D",
-          gray: "#2D2D2D",
-          lightgray: "#D9D9D9",
-          yellow: "#F2CB05",
-          black: "#1E1E1E",
-        },
+    colors: {
+      primary: {
+        darkgreen: "#038C4C",
+        lightgreen: "#04BF68",
+        darkblue: "#021B49",
+        white: "#FFFFEC",
+        darkgray: "#3D3D3D",
+        gray: "#2D2D2D",
+        lightgray: "#D9D9D9",
+        yellow: "#F2CB05",
+        black: "#1E1E1E",
       },
     },
+    extend: {},
     fontFamily: {
       inter: "Inter",
     },

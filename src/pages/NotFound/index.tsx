57125b53a6ca1f33c3fdc7ad36d0853a75715c94
IGOR  ASSIS BARONI <0221432311022@fatectq.local>
2024-10-09 12:09:09 -0300

    feat: not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,11 @@
-export function NotFound(){
-    return(
-        <div>Page Not Found</div>
+export function NotFound() {
+    return (
+        <div className="bg-primary-darkgreen w-screen h-screen font-inter flex items-center justify-center flex-col gap-3">
+            <p className="text-primary-yellow text-3xl font-extrabold text-shadow text-center">
+                iWORK
+            </p>
+            <p className="text-primary-yellow text-8xl font-extrabold text-shadow text-center">404</p>
+            <p className="text-primary-yellow text-3xl font-extrabold text-shadow text-center">Página não encontrada!</p>
+        </div>
     )
 }

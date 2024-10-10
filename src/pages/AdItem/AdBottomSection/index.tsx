6e4f52b85218eeb851722e8f00d4de7724090ee6
Igor Baroni <igorbaroni77@gmail.com>
2024-10-10 15:22:25 -0300

    feat: vendor pfp and name

--- src/pages/AdItem/AdBottomSection/index.tsx
+++ src/pages/AdItem/AdBottomSection/index.tsx
@@ -6,9 +6,22 @@ type AdProps = {
 
 export function AdBottomSection({ ad }: AdProps) {
   return (
-    <div className="p-5 flex gap-4 flex-col items-center md:items-stretch">
-      <p className="font-bold text-xl">Descrição do Anúncio</p>
-      <p className="font-medium text-xl">{ad.description}</p>
+    <div>
+      <div className="p-5 flex gap-4 flex-col items-center md:items-stretch">
+        <p className="font-bold text-xl">Vendedor</p>
+        <div className="flex gap-6 items-center">
+          <img
+            src="fotoPerfil"
+            alt="vendedor"
+            className="bg-primary-darkgray rounded-full size-24 object-cover"
+          />
+          <p className="font-medium text-xl">Nome do vendedor</p>
+        </div>
+      </div>
+      <div className="p-5 flex gap-4 flex-col items-center md:items-stretch">
+        <p className="font-bold text-xl">Descrição do Anúncio</p>
+        <p className="font-medium text-xl">{ad.description}</p>
+      </div>
     </div>
   );
 }

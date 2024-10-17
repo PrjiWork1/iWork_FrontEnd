464efeee35397deaf579ee69a2d22b6f337bad82
Igor Baroni <igorbaroni77@gmail.com>
2024-10-17 17:58:13 -0300

    fix: responsive header fixes

--- src/components/Header/HeaderMenu/index.tsx
+++ src/components/Header/HeaderMenu/index.tsx
@@ -36,34 +36,44 @@ export function HeaderMenu() {
         onClick={handleOpenMenu}
       />
 
-      {isMenuOpen && (
-        <div
-          className="bg-primary-lightgreen p-4 rounded absolute right-2 z-10 w-40 mt-1"
-          style={{ top: menuPosition.top }}
-        >
-          <div>
-            <div className="text-white flex flex-col gap-3">
-              <p className="text-primary-black font-medium">
-                Logado como <br />
-                <span className="font-semibold">{user?.completeName}</span>
-              </p>
-              <div className="border border-primary-darkgray"></div>
-              <Link
-                to={"/user"}
-                className="text-primary-black font-medium cursor-pointer"
-              >
-                Meu perfil
-              </Link>
-              <Link
-                to={"/login"}
-                className="text-primary-black font-medium cursor-pointer"
-              >
-                Sair
-              </Link>
-            </div>
+      <div
+        className={`bg-primary-lightgreen p-4 rounded absolute right-2 z-10 w-40 mt-1 transition-opacity duration-300 ease-in-out ${
+          isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+        style={{ top: menuPosition.top }}
+      >
+        <div>
+          <div className="text-white flex flex-col gap-3">
+            <p className="text-primary-black font-medium">
+              Logado como <br />
+              <span className="font-semibold">{user?.completeName}</span>
+            </p>
+            <p className="text-primary-yellow font-extrabold text-shadow visible sm:hidden">
+              iWork PRO
+            </p>
+            <div className="border border-primary-darkgray"></div>
+            <Link
+              to="/create-ad"
+              className="visible sm:hidden bg-primary-gray text-primary-yellow p-2 rounded-xl font-extrabold hover:bg-primary-yellow hover:text-primary-gray transition"
+            >
+              Anunciar
+            </Link>
+            <Link
+              to={"/user"}
+              className="text-primary-black font-medium cursor-pointer"
+            >
+              Meu perfil
+            </Link>
+
+            <Link
+              to={"/login"}
+              className="text-primary-black font-medium cursor-pointer"
+            >
+              Sair
+            </Link>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,7 +17,7 @@ export function Header() {
   }, []);
 
   return (
-    <div className="font-inter flex flex-col md:flex-row justify-between bg-primary-darkgreen px-10 py-3 items-center">
+    <div className="font-inter flex flex-row justify-between bg-primary-darkgreen px-5 md:px-10 py-3 items-center ">
       <Link to="/">
         <p className="text-primary-yellow text-3xl font-extrabold mx-3">
           iWORK
@@ -28,17 +28,17 @@ export function Header() {
         className="w-50 md:w-[40%] px-3 mt-3 md:mt-0 rounded outline-none focus:ring ring-primary-yellow/90 transition"
       /> */}
       <div
-        className={`flex flex-col md:flex-row text-center mt-3 md:mt-0 items-center  md:w-[30%] gap-2 md:gap-0 ${
+        className={`flex flex-row text-center items-center  md:w-[35%] gap-2 md:gap-0 ${
           isUserLogged ? "justify-between" : "justify-end"
         }`}
       >
         {isUserLogged && (
-          <p className="text-primary-yellow font-extrabold text-shadow">
+          <p className="text-primary-yellow font-extrabold text-shadow hidden sm:flex">
             iWork PRO
           </p>
         )}
         {isUserLogged && (
-          <Link to="/create-ad">
+          <Link to="/create-ad" className="hidden sm:flex">
             <button className="cursor-pointer bg-primary-gray text-primary-yellow p-2 rounded-xl font-extrabold hover:bg-primary-yellow hover:text-primary-gray transition">
               Anunciar
             </button>

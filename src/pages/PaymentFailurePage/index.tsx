0c680e7c471d1e58d4649c17d47143a6faf5c8bc
Igor Baroni <igorbaroni77@gmail.com>
2024-11-15 01:24:12 -0300

    feat: better design in success/failure pages

--- src/pages/PaymentFailurePage/index.tsx
+++ src/pages/PaymentFailurePage/index.tsx
@@ -1,14 +1,34 @@
 import { Link } from "react-router-dom";
 
 export function PaymentFailurePage() {
+  const handleReturnToHome = () => {
+    sessionStorage.removeItem("purchaseServiceData");
+  };
+
   return (
-    <div className="w-screen h-screen font-inter flex items-center justify-center flex-col">
-      <div className="border border-primary-black p-20 rounded-lg flex flex-col justify-around gap-10 items-center">
-        <p className="text-2xl font-semibold text-center flex gap-3">
-          Falha ao realizar a compra
+    <div className="bg-primary-lightgray w-screen h-screen font-inter flex items-center justify-center flex-col">
+      <div className="border bg-primary-white border-primary-black py-6 w-1/2 rounded-lg flex flex-col justify-around gap-10 items-center">
+        <svg
+          viewBox="0 0 24 24"
+          className="text-primary-red w-16 h-16 mx-auto my-6"
+        >
+          <circle cx="12" cy="12" r="12" fill="currentColor" />
+          <path
+            fill="white"
+            d="M15.78 8.22a1 1 0 0 1 0 1.414L13.414 12l2.364 2.364a1 1 0 0 1-1.414 1.414L12 13.414l-2.364 2.364a1 1 0 0 1-1.414-1.414L10.586 12 8.22 9.636a1 1 0 1 1 1.414-1.414L12 10.586l2.364-2.364a1 1 0 0 1 1.414 0z"
+          />
+        </svg>
+
+        <p className="text-2xl font-semibold text-center">
+          Falha ao realizar a compra.
         </p>
+
         <p>
-          <Link to="/" className="text-2xl font-semibold hover:underline">
+          <Link
+            to="/"
+            className="p-2 bg-primary-darkblue text-primary-white text-2xl font-semibold hover:bg-primary-darkblue/80 transition "
+            onClick={handleReturnToHome}
+          >
             Voltar à tela home
           </Link>
         </p>
--- src/pages/PaymentSuccessPage/index.tsx
+++ src/pages/PaymentSuccessPage/index.tsx
@@ -1,16 +1,61 @@
 import { Link } from "react-router-dom";
 
+type purchaseAdType = {
+  title: string;
+  unitprice: number;
+  id: string;
+};
+
 export function PaymentSucessPage() {
   const purchasedAd = sessionStorage.getItem("purchaseServiceData");
+  const data = JSON.parse(purchasedAd!);
+
+  const handleReturnToHome = () => {
+    sessionStorage.removeItem("purchaseServiceData");
+  };
 
   return (
-    <div className="w-screen h-screen font-inter flex items-center justify-center flex-col">
-      <div className="border border-primary-black p-20 rounded-lg flex flex-col justify-around gap-10 items-center">
-        <p className="text-2xl font-semibold text-center flex gap-3">
+    <div className="bg-primary-lightgray w-screen h-screen font-inter flex items-center justify-center flex-col">
+      <div className="border bg-primary-white border-primary-black py-6 w-1/2 rounded-lg flex flex-col justify-around gap-10 items-center">
+        <svg
+          viewBox="0 0 24 24"
+          className="text-primary-darkgreen w-16 h-16 mx-auto my-6"
+        >
+          <path
+            fill="currentColor"
+            d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
+          ></path>
+        </svg>
+        <p className="text-2xl font-semibold text-center">
           Compra realizada com sucesso!
         </p>
+        <div className="w-1/2 font-semibold text-lg flex flex-col gap-2">
+          {data.items.length > 1 && "Itens Adquiridos"}
+          {data.items.map((item: purchaseAdType) => (
+            <div>
+              <p>
+                Título do {data.items.length > 1 ? "item" : "serviço"}:{" "}
+                {item.title}
+              </p>
+              <p>
+                Preço do {data.items.length > 1 ? "item" : "serviço"}: R${" "}
+                {item.unitprice.toFixed(2)}
+              </p>
+            </div>
+          ))}
+          <p>Descrição informada: </p>
+          <textarea
+            className="resize-none w-full h-32 border-2 border-primary-darkgray rounded p-2"
+            value={data.description}
+            disabled
+          ></textarea>
+        </div>
         <p>
-          <Link to="/" className="text-2xl font-semibold hover:underline">
+          <Link
+            to="/"
+            className="p-2 bg-primary-darkblue text-primary-white text-2xl font-semibold hover:bg-primary-darkblue/80 transition "
+            onClick={handleReturnToHome}
+          >
             Voltar à tela home
           </Link>
         </p>
